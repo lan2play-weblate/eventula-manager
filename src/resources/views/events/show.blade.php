28b736de0d9089a3fc430475d180fe2ee5edc8a2
Mawiguk0 <markus.kohn@sund-xplosion.de>
2023-11-05 03:09:09 +0100

    Changed to cardbased Event Information

--- src/resources/views/events/show.blade.php
+++ src/resources/views/events/show.blade.php
@@ -94,8 +94,8 @@
                 <div class="col-md-4">
                     <div class="d-flex align-items-center">
                         <div>
-                            <p ><i class="fas fa-calendar-alt mr-2"></i>Start</p>
-                            <h3>{{ date('d.M Y', strtotime($event->start)) }} at {{ date('H:i', strtotime($event->start)) }} Uhr</h3>
+                            <p ><i class="fas fa-calendar-alt mr-2"></i> @lang('events.start')</p>
+                            <h3>{{ date('d.M Y', strtotime($event->start)) }} @lang('events.time_delimiter') {{ date('H:i', strtotime($event->start)) }} @lang('events.time_suffix')</h3>
                         </div>
                     </div>
                 </div>
@@ -103,8 +103,8 @@
                     <div class="d-flex align-items-center">
                         <div>
 							
-                            <p><i class="fas fa-calendar-times mr-2"></i>End</p>
-                            <h3>{{ date('d.M Y', strtotime($event->end)) }} at {{ date('H:i', strtotime($event->end)) }} Uhr</h3>
+                            <p><i class="fas fa-calendar-times mr-2"></i> @lang('events.end')</p>
+                            <h3>{{ date('d.M Y', strtotime($event->end)) }} @lang('events.time_delimiter') {{ date('H:i', strtotime($event->end)) }} @lang('events.time_suffix')</h3>
                         </div>
                     </div>
                 </div>
@@ -112,8 +112,8 @@
                     <div class="d-flex align-items-center">
                         <div>
 							
-                            <p><i class="fas fa-chair mr-2"></i>Capacity</p>
-                            <h3>{{ $event->capacity }} Seats</h3>
+                            <p><i class="fas fa-chair mr-2"></i> @lang('events.seatingcapacity')</p>
+                            <h3>{{ $event->capacity }} @lang('events.seating')</h3>
                         </div>
                     </div>
                 </div>
@@ -128,7 +128,7 @@
 			<!-- ICS Date Saver -->
             <div class="row">
                 <div class="col-12">
-				<a href="{{ route('generate-event-ics', ['event' => $event]) }}" download><i class="fas fa-calendar-alt mr-2"></i>Save to Calendar</a>
+				<a href="{{ route('generate-event-ics', ['event' => $event]) }}" download><i class="fas fa-calendar-alt mr-2"></i> Save to Calendar</a>
                 </div>
             </div>
         </div>
