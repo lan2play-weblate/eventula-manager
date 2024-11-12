9bc351fbaba6bddc38c9edb9ba5ce8cd9a754018
Alexander Volz <Alexander@volzit.de>
2024-11-13 00:42:06 +0100

    fix event card title and spacing in event index

--- src/resources/views/events/index.blade.php
+++ src/resources/views/events/index.blade.php
@@ -1,21 +1,19 @@
 @extends ('layouts.default')
 
-@section ('page_title', Settings::getOrgName() . ' - Events')
+@section('page_title', Settings::getOrgName() . ' - Events')
 
-@section ('content')
-<!-- I know, this card under a card is not cool, but i wont duplicate the information-card for a unused index page of events ;-) -->
-<div class="container pt-1">
-	<div class="pb-2 mt-4 mb-4 border-bottom">
-		<h1>
-			Events
-		</h1>
-	</div>
-	@foreach ($events as $event)	
-	<div class="card card-header">
-	<strong><h2 href="/events/{{ $event->slug }}">{{ $event->display_name }}</h2></strong>
-	</div>
-	@include ('layouts._partials._events.information-card')
-	@endforeach
-</div>
+@section('content')
+    <div class="container pt-1">
+        <div class="pb-2 mt-4 mb-4 border-bottom">
+            <h1>
+                Events
+            </h1>
+        </div>
+        @foreach ($events as $event)
+		<div class="mb-3">
+            @include ('layouts._partials._events.information-card', ['enableCardTitle' => true])
+		</div>
+        @endforeach
+    </div>
 
 @endsection
--- src/resources/views/layouts/_partials/_events/information-card.blade.php
+++ src/resources/views/layouts/_partials/_events/information-card.blade.php
@@ -1,4 +1,13 @@
 <div class="card shadow">
+    @if ($enableCardTitle ?? false)
+        <a href="/events/{{ $event->slug }}">
+            <div class="card-header">
+                <strong>
+                    <h2>{{ $event->display_name }}</h2>
+                </strong>
+            </div>
+        </a>
+    @endif
     <div class="card-body">
         <!-- Start and End Info -->
         <div class="row mb-4">
@@ -24,9 +33,13 @@
             <div class="col-md-4">
                 <div class="d-flex align-items-center">
                     <div>
-
-                        <p><i class="fas fa-chair mr-2"></i> @lang('events.seatingcapacity')</p>
-                        <h3>{{ $event->capacity }} @lang('events.seating')</h3>
+                        @if ($event->getSeatingCapacity() == 0)
+                            <p><i class="fas fa-people-group mr-2"></i> @lang('events.capacity')</p>
+                            <h3>{{ $event->capacity }}</h3>
+                        @else
+                            <p><i class="fas fa-chair mr-2"></i> @lang('events.seatingcapacity')</p>
+                            <h3>{{ $event->getSeatingCapacity() }} </h3>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -34,7 +47,7 @@
         <!-- Description -->
         <div class="row">
             <div class="col-12">
-                <p>{{ $event->desc_long }}</p>
+                <p>{!! $event->desc_long !!}</p>
             </div>
         </div>
 
